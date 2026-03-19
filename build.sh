894631faac67571e014fe1b5a222b3ac9ea4b26c
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-03-18 23:21:18 -0700

    ci: update github deployment workflow and build scripts

--- build.sh
+++ build.sh
@@ -2,38 +2,29 @@
 
 #------------------------------------------------------------------------------
 # @file
-# Builds a Hugo site hosted on a Cloudflare Worker.
-#
-# The Cloudflare Worker automatically installs Node.js dependencies.
+# Builds a Hugo site with Webpack and Cloudflare Worker.
 #------------------------------------------------------------------------------
 
 main() {
 
   DART_SASS_VERSION=1.98.0
-  GO_VERSION=1.26.1
-  HUGO_VERSION=0.158.0
-  NODE_VERSION=24.14.0
+  HUGO_VERSION=0.157.0
+  NODE_VERSION=22.22.1
 
   export TZ=Europe/Oslo
 
   # Install Dart Sass
   echo "Installing Dart Sass ${DART_SASS_VERSION}..."
   curl -sLJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  mkdir -p "${HOME}/.local/bin"
   tar -C "${HOME}/.local" -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
   rm "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
   export PATH="${HOME}/.local/dart-sass:${PATH}"
 
-  # Install Go
-  echo "Installing Go ${GO_VERSION}..."
-  curl -sLJO "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
-  tar -C "${HOME}/.local" -xf "go${GO_VERSION}.linux-amd64.tar.gz"
-  rm "go${GO_VERSION}.linux-amd64.tar.gz"
-  export PATH="${HOME}/.local/go/bin:${PATH}"
-
   # Install Hugo
   echo "Installing Hugo ${HUGO_VERSION}..."
   curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
-  mkdir "${HOME}/.local/hugo"
+  mkdir -p "${HOME}/.local/hugo"
   tar -C "${HOME}/.local/hugo" -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
   rm "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
   export PATH="${HOME}/.local/hugo:${PATH}"
@@ -45,23 +36,25 @@ main() {
   rm "node-v${NODE_VERSION}-linux-x64.tar.xz"
   export PATH="${HOME}/.local/node-v${NODE_VERSION}-linux-x64/bin:${PATH}"
 
+  # Corepack for Yarn
+  echo "Enabling Corepack for Yarn..."
+  corepack enable
+  corepack prepare yarn@4.13.0 --activate
+
   # Verify installations
   echo "Verifying installations..."
   echo Dart Sass: "$(sass --version)"
-  echo Go: "$(go version)"
   echo Hugo: "$(hugo version)"
   echo Node.js: "$(node --version)"
+  echo Yarn: "$(yarn --version)"
 
-  # Configure Git
-  echo "Configuring Git..."
-  git config core.quotepath false
-  if [ "$(git rev-parse --is-shallow-repository)" = "true" ]; then
-    git fetch --unshallow
-  fi
+  # Install dependencies
+  echo "Installing dependencies..."
+  yarn install --immutable
 
   # Build the site
   echo "Building the site..."
-  hugo build --gc --minify
+  yarn run build:all
 
 }
 
