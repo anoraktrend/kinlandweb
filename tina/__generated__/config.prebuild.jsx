dbf7728134ff574c292f2022f8b0c1335c660427
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-06-24 21:15:55 -0700

    fix: commit Tina generated client for production builds

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,57 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: null,
+  token: null,
+  build: {
+    publicFolder: "public",
+    outputFolder: "admin"
+  },
+  media: {
+    tina: {
+      publicFolder: "public",
+      mediaRoot: "img"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/post",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            required: true,
+            isTitle: true
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
