e7caa54d073130c40e7ed50021f816342cfab085
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-06-24 21:12:25 -0700

    chore: update configs for Astro v7, Cloudflare Workers, and TinaCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import tina from "@tinacms/astro/integration";
+import { tinaAdminDevRedirect } from "@tinacms/astro/vite";
 
 export default defineConfig({
-  site: 'https://kinland.helltop.net',
+  site: "https://kinland.helltop.net",
+  output: "server",
   adapter: cloudflare({
-    imageService: 'passthrough',
+    imageService: "passthrough",
   }),
+  integrations: [tina()],
+  vite: {
+    plugins: [tinaAdminDevRedirect()],
+    ssr: {
+      noExternal: ["@tinacms/astro", "@tinacms/bridge"],
+    },
+  },
 });
